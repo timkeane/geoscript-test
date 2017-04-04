8598c7e878d7038e1d97d13bac30d7f1a2898d11
Tim Keane <t.d.keane@gmail.com>
2017-04-04 14:01:25 -0400

    Add files via upload

---
+++ src/test/groovy/nyc/FeatureIdTest.groovy
@@ -0,0 +1,23 @@
+package nyc
+
+import spock.lang.*
+
+import geoscript.feature.*
+import geoscript.layer.Layer
+import geoscript.geom.*
+
+class ProximityTest extends Specification {
+
+	def 'test that feature ids persist'(){
+		given:
+			Schema schema = new Schema('mySchema', [['geom', 'Point'], ['name', 'string'], ['address', 'string']])
+			def attrs = [geom: new Point(1, 10), name: 'myName', address: 'myAddress']
+			Feature inFeature = new Feature(attrs, 'myFID', schema)
+			Layer layer = new Layer('myLayer', schema)
+			layer.add(inFeature)
+		when:
+			Feature outFeature = layer.getFeatures()[0]
+		then:
+			inFeature.getId() == outFeature.getId() 
+	}
+}
